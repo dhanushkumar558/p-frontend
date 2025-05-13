2e1de52de34d30749759421100fabca19d773b6e
Dhanush Kumar V <your_email@example.com>
2025-05-13 10:34:10 +0530

    Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,24 +9,24 @@ export default function Footer() {
           <div className="col-md-4">
             <h5 className="footer-title">Certifications</h5>
             <ul className="social-links">
-              <li><a href="#" className="social-icon">React Developer</a></li>
-              <li><a href="#" className="social-icon">Full Stack Developement</a></li>
-              <li><a href="#" className="social-icon">Mysql</a></li>
+              <li><a href="https://ik.imagekit.io/idewrsin9/full.jpg?updatedAt=1747112005825" className="social-icon">Full Stack Developement</a></li>
+              <li><a href="https://ik.imagekit.io/idewrsin9/UC-bf995f8b-a492-4a0f-8637-c1aaecf45e8b.jpg?updatedAt=1747111912992" className="social-icon">React Developer</a></li>
+              <li><a href="https://ik.imagekit.io/idewrsin9/sql.jpg?updatedAt=1747112108312" className="social-icon">Mysql</a></li>
             </ul>
           </div>
           <div className="col-md-4">
             <h5 className="footer-title">Follow Me</h5>
             <ul className="social-links">
-              <li><a href="#" className="social-icon">Facebook</a></li>
-              <li><a href="#" className="social-icon">Twitter</a></li>
-              <li><a href="#" className="social-icon">Instagram</a></li>
+              <li><a href="https://www.facebook.com/CeoOfTredex" className="social-icon">Facebook</a></li>
+              <li><a href="https://www.instagram.com/the_hyperguy_official/" className="social-icon">Instagram</a></li>
+              <li><a href="https://x.com/darlzyking" className="social-icon">Twitter</a></li>
             </ul>
           </div>
           <div className="col-md-4">
             <h5 className="footer-title">Contact</h5>
             <ul className="social-links">
-              <li><a href="#" className="social-icon">Mail</a></li>
-              <li><a href="#" className="social-icon">Github</a></li>
+              <li><a href="https://www.linkedin.com/in/dhanush-kumar558/" className="social-icon">LinkedIn</a></li>
+              <li><a href="https://github.com/dhanushkumar558?tab=repositories" className="social-icon">Github</a></li>
               
             </ul>
           </div>
